9e26e9c3b538cc3bc84b9a3cb102acbfb2ab295f
Alois Sečkár <ellrohir@seznam.cz>
2023-09-06 22:40:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Nuxtplorer
+Demo of a mini-RPG game.
+
+Origially based on a simple idea of moving dot presented in [Vue School lesson about handling the keyboard events with VueUse](https://vueschool.io/lessons/the-keyboard-and-vueuse).
---
+++ app.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="ball" />
+</template>
+
+<script setup lang="ts">
+import { onKeyStroke } from '@vueuse/core'
+
+const controls = {
+  ArrowDown: () => (position.value.y += 5),
+  ArrowUp: () => (position.value.y -= 5),
+  ArrowRight: () => (position.value.x += 5),
+  ArrowLeft: () => (position.value.x -= 5)
+}
+
+onKeyStroke(Object.keys(controls), (e) => {
+  e.preventDefault()
+  controls[e.key]()
+})
+
+const position = ref({ x: 0, y: 0 })
+</script>
+
+<style>
+.ball {
+  width: 10px;
+  height: 10px;
+  background: red;
+  border-radius: 50%;
+  position: absolute;
+  top: v-bind(position.y + 'px');
+  left: v-bind(position.x + 'px');
+}
+</style>
