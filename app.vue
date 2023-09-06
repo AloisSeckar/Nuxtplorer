ce456a91dc36d75e418b865c37b8d7851f412528
Alois Sečkár <ellrohir@seznam.cz>
2023-09-06 23:36:31 +0200

    hero moving animation

--- app.vue
+++ app.vue
@@ -3,31 +3,86 @@
 </template>
 
 <script setup lang="ts">
-import { onKeyStroke, useWindowSize } from '@vueuse/core'
+import { onKeyStroke, onKeyUp, useRafFn, useWindowSize } from '@vueuse/core'
 
 const controls = {
-  ArrowDown: () => (position.value.y += 5),
-  ArrowUp: () => (position.value.y -= 5),
-  ArrowRight: () => (position.value.x += 5),
-  ArrowLeft: () => (position.value.x -= 5)
+  ArrowDown: () => {
+    position.value.y += 5
+    direction.value = 'd'
+    offset.value.y = 128
+  },
+  ArrowUp: () => {
+    position.value.y -= 5
+    direction.value = 'u'
+    offset.value.y = 0
+  },
+  ArrowRight: () => {
+    position.value.x += 5
+    direction.value = 'r'
+    offset.value.y = 192
+  },
+  ArrowLeft: () => {
+    position.value.x -= 5
+    direction.value = 'l'
+    offset.value.y = 64
+  }
 }
+
+useRafFn(() => {
+  if (moving.value) {
+    frames.value++
+    if (frames.value % 15 === 0) {
+      switch (phase.value) {
+        case 1:
+          phase.value = 2
+          offset.value.x = 48
+          break
+        case 2:
+          phase.value = 3
+          offset.value.x = 96
+          break
+        case 3:
+          phase.value = 1
+          offset.value.x = 0
+          break
+      }
+    }
+  }
+})
 
 onKeyStroke(Object.keys(controls), (e) => {
   e.preventDefault()
+  moving.value = true
   controls[e.key]()
 })
 
+onKeyUp(Object.keys(controls), (e) => {
+  e.preventDefault()
+  moving.value = false
+  frames.value = 0
+  phase.value = 2
+})
+
 const { width, height } = useWindowSize()
-const heroWidth = 30
-const heroHeight = 52
+
+const heroWidth = 48
+const heroHeight = 64
+
+const offset = ref({ x: 48, y: 128 })
 const position = ref({ x: width.value / 2 - heroWidth / 2, y: height.value / 2 - heroHeight / 2 })
+
+const direction: Ref<'u' | 'd' | 'l' | 'r'> = ref('d')
+
+const moving = ref(false)
+const frames = ref(0)
+const phase: Ref<1 | 2 | 3> = ref(2)
 </script>
 
 <style>
 .hero {
   width: v-bind(heroWidth + 'px');
   height: v-bind(heroHeight + 'px');
-  background-image: url('hero.png');
+  background: url('hero-sprite.png') v-bind(offset.x + 'px') v-bind(offset.y + 'px');
   position: absolute;
   top: v-bind(position.y + 'px');
   left: v-bind(position.x + 'px');
