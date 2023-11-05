820bb151900727349b96ff1763c11811cd5e9698
Alois Sečkár <ellrohir@seznam.cz>
2023-11-05 20:00:24 +0100

    feat: upgrade gems sprite

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="gem gem-1" style="left: 142px; top: 132px;" />
-    <div class="gem gem-2" style="left: 140px; top: 258px;" />
+    <div class="gem gem-1" style="left: 129px; top: 129px;" />
+    <div class="gem gem-2" style="left: 129px; top: 257px;" />
     <ClientOnly>
       <div class="overlay">
         <GuiCoordsBox />
