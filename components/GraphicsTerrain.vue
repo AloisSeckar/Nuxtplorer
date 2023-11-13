31d68d34c7017eef9706b8e445b897e21474c619
Alois Sečkár <ellrohir@seznam.cz>
2023-11-13 22:06:36 +0100

    feat: css transform background only

--- components/GraphicsTerrain.vue
+++ components/GraphicsTerrain.vue
@@ -34,47 +34,147 @@ const terrainHeight = computed(() => useTerrainStore().getHeight)
   height: v-bind('TILE_SIZE + "px"');
   display: inline-block;
 }
+
 .stone {
   background: url('caverns.png') 0px 0px;
 }
+
 .corridor-lr {
+  position: relative;
+}
+.corridor-lr::before {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  content: '';
   background: url('caverns.png') 256px 0px;
 }
+
 .corridor-td {
+  position: relative;
+}
+.corridor-td::before {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  content: '';
   background: url('caverns.png') 256px 0px;
   transform: rotate(90deg);
 }
+
 .turn-dr {
+  position: relative;
+}
+.turn-dr::before {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  content: '';
   background: url('caverns.png') 192px 0px;
 }
+
 .turn-dl {
+  position: relative;
+}
+.turn-dl::before {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  content: '';
   background: url('caverns.png') 192px 0px;
   transform: rotate(90deg);
 }
+
 .turn-tl {
+  position: relative;
+}
+.turn-tl::before {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  content: '';
   background: url('caverns.png') 192px 0px;
   transform: rotate(180deg);
 }
+
 .turn-tr {
+  position: relative;
+}
+.turn-tr::before {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  content: '';
   background: url('caverns.png') 192px 0px;
   transform: rotate(270deg);
 }
+
 .cross-3-d {
+  position: relative;
+}
+.cross-3-d::before {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  content: '';
   background: url('caverns.png') 128px 0px;
 }
+
 .cross-3-l {
+  position: relative;
+}
+.cross-3-l::before {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  content: '';
   background: url('caverns.png') 128px 0px;
   transform: rotate(90deg);
 }
+
 .cross-3-t {
+  position: relative;
+}
+.cross-3-t::before {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  content: '';
   background: url('caverns.png') 128px 0px;
   transform: rotate(180deg);
 }
+
 .cross-3-r {
+  position: relative;
+}
+.cross-3-r::before {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  content: '';
   background: url('caverns.png') 128px 0px;
   transform: rotate(270deg);
 }
+
 .cross-4 {
+  position: relative;
+}
+.cross-4::before {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  content: '';
   background: url('caverns.png') 64px 0px;
 }
 </style>
